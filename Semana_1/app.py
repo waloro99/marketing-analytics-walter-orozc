291110b2e0ff69e04d547815e81554e1556c4d21
waloro99 <walterof1@hotmail.com>
2026-09-24 18:56:32 -0600

    App terminada

--- Semana_1/app.py
+++ Semana_1/app.py
@@ -1,1 +1,15 @@
 # Archivo base para el despliegue del Agente en Streamlit
+import streamlit as st
+from sklearn.linear_model import LinearRegression
+import numpy as np
+st.title("Configuracion inicial")
+st.write("Primera prueba de uso de streamlit y ambiente de MA2026")
+gasto=st.slider("Seleccine nivel de gasto en publicicdad", 10,200,50)
+variable_x = np.array([[10], [20], [30], [40],[50]])
+variable_y = np.array([15,25,35,45,55])
+modelo_lr = LinearRegression()
+modelo_lr.fit(variable_x,variable_y)
+if st.button("Predecir"):
+ resultado = modelo_lr.predict([[gasto]])
+ st.success(f"Las ventas proyectadas para una inversion de ${gasto} son:
+${resultado[0]}")
